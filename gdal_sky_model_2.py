27f682296910507f82b1c865fc19e193394a7997
Jacob Adams <jacob.adams@cachecounty.org>
2018-04-09 12:40:43 -0600

    updating skymodel documentation

--- gdal_sky_model_2.py
+++ gdal_sky_model_2.py
@@ -1,11 +1,11 @@
 #*****************************************************************************
-# 
+#
 #  Project:  GDAL Skyshade
 #  Purpose:  Create "skyshade" hillshades using GDAL using the method described
-#            in Kennelly and Stewart (2014), General sky models for 
+#            in Kennelly and Stewart (2014), General sky models for
 #            illuminating terrains.
 #  Author:   Jacob Adams, jacob.adams@cachecounty.org
-# 
+#
 #*****************************************************************************
 # MIT License
 #
@@ -18,7 +18,7 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in 
+# The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -26,7 +26,7 @@
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #*****************************************************************************
 
@@ -45,7 +45,7 @@ import os
 
 def hillshade(array, az, alt, p_height, p_width, scale):
     '''
-    Hillshade algorithm based on comments in the gdal DEMProcessing routines. 
+    Hillshade algorithm based on comments in the gdal DEMProcessing routines.
     '''
 
     x = np.zeros(array.shape)
@@ -55,20 +55,30 @@ def hillshade(array, az, alt, p_height, p_width, scale):
     altrad = alt * np.pi/180.
 
     x, y = np.gradient(array, p_width, p_height, edge_order=2)
-    
+
     # sinalt = np.sin(altrad)
     # cosaz = np.cos(azrad)
     # cosalt = np.cos(altrad)
     # sinaz = np.sin(azrad)
-    # xx_plus_yy = x*x + y*y   
+    # xx_plus_yy = x*x + y*y
     # shaded = (sinalt - (y * cosaz * cosalt - x * sinaz * cosalt)) / np.sqrt(1+xx_plus_yy)
 
     shaded = (np.sin(altrad) - (y * np.cos(azrad) * np.cos(altrad) - x * np.sin(azrad) * np.cos(altrad))) / np.sqrt(1+(x*x + y*y))
-    
-    return shaded*255    
-    
+
+    return shaded*255
+
 def skyshade(in_dem, lum_file, out_path, temp_dir, ex_factor, size_limit):
-    
+    '''
+    Implements sky model as described in Kennelly and Stewart (2014), General
+    sky models for illuminating terrains.
+    in_dem:         Path to input DEM
+    lum_file:       Path to csv file containing az/alt/weight lines
+    out_path:       Path for completed file
+    temp_dir:       Path to directory that will hold the pickled chunks
+    ex_factor:      Exageration factor
+    size_limit:     Chunk size
+    '''
+
     # Load csv into lines array
     # break DEM into chunks, pickle chunks
     # for each chunk:
@@ -78,9 +88,9 @@ def skyshade(in_dem, lum_file, out_path, temp_dir, ex_factor, size_limit):
     #       add hillshade to existing running sum
     #   save chunk
     # Recombine chunks, save as output
-    
+
     pickle_dir = temp_dir
-    
+
     gdal.UseExceptions()
 
     start = datetime.datetime.now()
@@ -98,10 +108,10 @@ def skyshade(in_dem, lum_file, out_path, temp_dir, ex_factor, size_limit):
     yOrigin = transform[3]
     pixelWidth = transform[1]
     pixelHeight = transform[5]
-    
+
     # read in data (only one band in dem) and store as array
     elev_data = ds.ReadAsArray()
-    
+
     elev_data = elev_data * ex_factor
 
     # Read in luminance file, loop line by line
@@ -111,22 +121,22 @@ def skyshade(in_dem, lum_file, out_path, temp_dir, ex_factor, size_limit):
         reader = csv.reader(l)
         for line in reader:
             lines.append(line)
-    
+
     # Split dem into chunks for processing
     print("Splitting DEM into chunks of %d by %d pixels" %(size_limit, size_limit))
-    sub_arrays = {} 
+    sub_arrays = {}
     keys = []
     if rows > size_limit and cols > size_limit:
         #Split it
-        
+
         # calculate breaks every size_limit pixels
         row_splits = range(0, rows, size_limit)
         col_splits = range(0, cols, size_limit)
-        
+
         # add total number of rows/cols to be last break
         row_splits.append(rows)
         col_splits.append(cols)
-        
+
         # Split into 2000x2000 pixel chunks
         for i in range(len(row_splits)):
             if i < len(row_splits)-1:
@@ -140,16 +150,16 @@ def skyshade(in_dem, lum_file, out_path, temp_dir, ex_factor, size_limit):
                         for k in np.nditer(data, op_flags=['readwrite']):
                             if k < 0:
                                 k[...] = -999.
-                        np.save(os.path.join(pickle_dir, key), data)        
-        
+                        np.save(os.path.join(pickle_dir, key), data)
+
     else:
         key = "0;%d;0;%d" % (rows, cols)
         keys.append(key)
         np.save(os.path.join(pickle_dir, key), elev_data)
 
     # Memory managment
-    elev_data = None    
-        
+    elev_data = None
+
     # Calculate skyshade for each subarray
     print("Looping through chunks")
     skyshades = []
@@ -159,21 +169,21 @@ def skyshade(in_dem, lum_file, out_path, temp_dir, ex_factor, size_limit):
         print("Chunk %s, %d of %d" %(key, counter, len(keys)))
         pickle_path = os.path.join(pickle_dir, key) + ".npy"
         chunk = np.load(pickle_path)
-        
+
         # initialize total skyshade for this chunk as 0's
         skyshade_chunk = np.zeros((chunk.shape))
-        
+
         # Loop through luminance file lines to calculate multiple hillshades for that chunk
         for line in lines:
             az = float(line[0])
             alt = float(line[1])
             weight = float(line[2])
-            
+
             # Modify azimuth to proper value for maths
             az = 180. - az
             if az < 0.:
                 az = az + 360.
-            
+
             tfile_source = "c:\\temp\\gis\\skyshade\\temp\\tempsource.tif"
             tfile_dest = "c:\\temp\\gis\\skyshade\\temp\\tempdest.tif"
             # Must specify gdal.GDT_Float32 as the data type here, otherwise it will default to 8-bit and lose all information
@@ -185,9 +195,9 @@ def skyshade(in_dem, lum_file, out_path, temp_dir, ex_factor, size_limit):
             temp_ds.SetProjection(projection)
 
             shade = gdal.DEMProcessing(tfile_dest, temp_ds, "hillshade", azimuth = az, altitude = alt, computeEdges = True, scale = pixelWidth).ReadAsArray() * weight#.astype(np.uint8)
-            
+
             temp_ds = None
-            
+
             # calculate a new hillshade array, multiplied by weight
             #shade = hillshade(chunk, az, alt, pixelHeight, pixelWidth, 1) * weight
 
@@ -197,28 +207,28 @@ def skyshade(in_dem, lum_file, out_path, temp_dir, ex_factor, size_limit):
             # sum the new hillshade array with the sum of the previous hillshades array
             #total = previous + shade
 
-            # copy (deep copy? reference? may cause headaches...) for next iteration 
+            # copy (deep copy? reference? may cause headaches...) for next iteration
             #previous = total
-            
+
             skyshade_chunk = skyshade_chunk + shade
-            print('.', end='')     
-        
+            print('.', end='')
+
         # new keys (filenames for completed chunks) have ';99999' appended; appended part must be an int to work with the map(int, ...) below.
         shadedkey = key + ";99999"
         skyshades.append(shadedkey)
-        
+
         np.save(os.path.join(pickle_dir, shadedkey), skyshade_chunk)
         print("")
-    
+
     #Rebuild into one large array
     print("Rebuilding chunks into %s" %(out_path))
-    final = np.zeros((rows, cols))#.astype(Byte)   
+    final = np.zeros((rows, cols))#.astype(Byte)
     for part in skyshades:
         sky_path = os.path.join(pickle_dir, part) + ".npy"
         skyshade = np.load(sky_path)
         s = map(int, part.split(';'))
         final[s[0]:s[1],s[2]:s[3]] = skyshade
-        
+
     # Write out array as final skyshade
     dest = driver.Create(out_path, cols, rows, 1, gdal.GDT_Float32)
     band = dest.GetRasterBand(1)
@@ -229,7 +239,7 @@ def skyshade(in_dem, lum_file, out_path, temp_dir, ex_factor, size_limit):
     # Delete objects, causing destination data set to write to disk. Must delete
     # band before dataset to prevent gdal gotcha
     band = None
-    dest = None    
+    dest = None
 
     finish = datetime.datetime.now() - start
     print("Total time: " + str(finish))
@@ -246,7 +256,7 @@ out_dir = "c:\\temp\\gis\\skyshade\\skyshades"
 temp_dir = "c:\\temp\\gis\\skyshade\\temp"
 lum = "c:\\temp\\gis\\skyshade\\lum\\1_45_315_150.csv"
 out = out_dir + "\\1_45_315_150.tif"
-   
+
 skyshade(dem, lum, out, temp_dir, ex, size)
 
 skyshade(dem, "c:\\temp\\gis\\skyshade\\lum\\2_45_315_150.csv", out_dir + "\\2_45_315_150.tif", temp_dir, ex, size)
@@ -258,7 +268,7 @@ skyshade(dem, "c:\\temp\\gis\\skyshade\\lum\\5_45_315_150.csv", out_dir + "\\5_4
     # lum = "c:\\temp\\gis\\skyshade\\lum\\" + filename
     # out = "c:\\temp\\gis\\skyshade\\out\\" + str(i) + "_45_215.tif"
     # skyshade(dem, lum, out, ex, size)
-    
+
 # for path, x, name in os.walk("C:\\temp\\gis\\skyshade\\lum"):
     # for fname in name:
         # if fname.partition('.')[2] == 'csv':
@@ -268,6 +278,3 @@ skyshade(dem, "c:\\temp\\gis\\skyshade\\lum\\5_45_315_150.csv", out_dir + "\\5_4
             # out = os.path.join(out_dir, tif)
             # print("Running: " + no_csv)
             # skyshade(dem, lum, out, temp_dir, ex, size)
-        
-        
-
