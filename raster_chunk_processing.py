ec72549c4dc6f0517f7471ec3561200f472f0c7e
Jacob Adams <jacob.adams@cachecounty.org>
2018-04-12 10:57:59 -0600

    rcp: adding straight scaling to skymodel

--- raster_chunk_processing.py
+++ raster_chunk_processing.py
@@ -312,7 +312,17 @@ def skymodel(in_array, lum_lines):
 
         skyshade = skyshade + shade
         shade = None
-    return skyshade
+
+    # Scale to 1-255
+    # ((newmax-newmin)(val-oldmin))/(oldmax-oldmin)-newmin
+    newmax = 255
+    newmin = 1
+    oldmax = np.nanmax(skyshade)
+    oldmin = np.nanmin(skyshade)
+
+    scaled = (newmax - newmin) * (skyshade-oldmin) / (oldmax - oldmin) - newmin
+
+    return scaled
 
 
 def TPI(in_array, filter_size):
@@ -696,6 +706,8 @@ def ParallelRCP(in_dem_path, out_dem_path, chunk_size, overlap, method,
     t_band = None
     t_fh = None
 
+    # If we're doing a skymodel, we need to read in the whole luminance file
+    # and add that list to the options dictionary
     if method == "skymodel":
         if verbose:
             print("Reading in luminance file {}".format(options["lum_file"]))
@@ -1091,9 +1103,9 @@ if "__main__" in __name__:
     #hs_dem = "c:\\temp\\gis\\hstest\\dem_state_gauss30_sky.tif"
     lum = "c:\\temp\\gis\\skyshade\\lum\\1_45_315_150.csv"
 
-    #in_dem = "e:\\lidar\\canyons\\dem\\merged_raw_dem.vrt"
-    smooth_dem = "e:\\lidar\\canyons\\dem\\CCDEM-ft_md506050-lzw.tif"
-    hs_dem = "e:\\lidar\\canyons\\dem\\CCDEM-ft_md506050_skymodel.tif"
+    in_dem = "e:\\lidar\\canyons\\dem\\CCDEM-ft-lzw.tif"
+    smooth_dem = "e:\\lidar\\canyons\\dem\\CCDEM-ft_gauss30.tif"
+    #hs_dem = "e:\\lidar\\canyons\\dem\\CCDEM-ft_md506050_skymodel.tif"
 
     # md105060 = n=10, t=0.50, v=60
 
@@ -1106,9 +1118,9 @@ if "__main__" in __name__:
 
     #RCProcessing(in_dem, smooth_dem, window_size, filter_f, "mdenoise", {"n":n, "t":t, "v":v})
     #ParallelRCP(in_dem, smooth_dem, window_size, filter_f, "mdenoise", {"n":n, "t":t, "v":v}, 3, False)
-    #ParallelRCP(in_dem, smooth_dem, window_size, filter_f, "blur_gauss", {"filter_size":30}, 3, True)
+    ParallelRCP(in_dem, smooth_dem, window_size, filter_f, "blur_gauss", {"filter_size":30}, 3, True)
     #ParallelRCP(in_dem, smooth_dem, window_size, filter_f, "TPI", {"filter_size":60}, num_threads=4, verbose=True)
-    ParallelRCP(smooth_dem, hs_dem, 4000, filter_f, "skymodel", {"lum_file":lum}, num_threads=3, verbose=True)
+    #ParallelRCP(smooth_dem, hs_dem, 4000, filter_f, "skymodel", {"lum_file":lum}, num_threads=3, verbose=True)
     # times = {}
     # for i in range(1, 11, 1):
     #     smooth_dem = "c:\\temp\\gis\\dem_state_ParallelRCPTest_{}.tif".format(i)
